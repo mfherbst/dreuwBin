1c2944cc5ee27dd12601e06937448ffe927b6272
Michael F. Herbst <info@michael-herbst.com>
2015-04-09 11:35:39 +0200

    Added copyright, disclaimer, licensing information
    All is GPL 3 of cause ;)

--- bash_completion.d/qsubi.bash
+++ bash_completion.d/qsubi.bash
@@ -1,5 +1,6 @@
 _qsubi() {
 	local cur
+	local prev
 	prev=${COMP_WORDS[$((COMP_CWORD-1))]}
 	cur=${COMP_WORDS[COMP_CWORD]}
 
@@ -8,6 +9,8 @@ _qsubi() {
 
 	#generate reply using options	
 	COMPREPLY=( $( compgen -W '-h --help --np --wt --mem' -- "$cur" ) )
+
+	unset cur prev
 }
 
 complete -F _qsubi qsubi
