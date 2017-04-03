12075cd9f53952281e64598834cf6757c35b1640
Michael F. Herbst <info@michael-herbst.com>
2017-04-03 11:26:53 +0200

    QSYS lines now have higher priority than input parameters
    This is to make sure that arguments the user specifies always have
    higher priority than stuff the program reads.

--- queuing_system/qsys_line.py
+++ queuing_system/qsys_line.py
@@ -16,7 +16,7 @@
 # A copy of the GNU General Public License can be found in the
 # file LICENCE or at <http://www.gnu.org/licenses/>.
 
-from queuing_system.queuing_system_data import queuing_system_data
+from queuing_system.queuing_system_data import queuing_system_data, node_type
 from argparse import ArgumentParser
 import shared_utils_lib as utils
 import re
@@ -25,13 +25,9 @@ class qsys_line:
     """
     Class to parse or build a QSYS line originating
     from an input file of another program
-
-    The #QSYS, !QSYS or whatever prefix should already
-    be stripped by the caller. This function only interprets
-    the plain key-value pairs like
-    "wt=4h  mem=3gb" ...
     """
-    def __parse_to_key_value(self,line):
+
+    def __parse_to_key_value(line):
         # Normalise:
         line=re.sub("\W*=\W*","=", line)
 
@@ -44,77 +40,114 @@ class qsys_line:
         # Make key-value pairs:
         return { v[0].strip() : v[1].strip() for v in kv_raw if len(v) == 2 }
 
-    def __init__(self,line):
-        """Initialise with the line to parse"""
-        self.__vals = self.__parse_to_key_value(line)
-
-    def __print_ignore(self,directive):
-        print("Warning: Ignoring "+qsys_line.available_directives[directive]
-                + " (== " + self.__vals[directive] + ") "
-                + "specified in input file "
-                + "via \"QSYS "+directive+"=\", "
-                + "since already given on commandline or in input file.")
-
-    def parse_into(self,data):
-        """Place the values in this data object"""
-
+    def __init__(self,data):
+        """Initialise with the data object to place the results"""
         if not isinstance(data,queuing_system_data):
             raise TypeError("data should be of type queuing_system_data")
 
-        for k in self.__vals:
-            verror_string = ("Could not parse value \""+self.__vals[k]
+        self.__data = data
+
+    def parse_file(self, f, comment_chars=["#"], keywords=["QSYS"]):
+        """File to parse and place the values in the data object,
+           which was provided upon construction.
+           Only values which are Null will not be set.
+
+           All characters in a line before comment_char + keyword or
+           comment_char + " " + keyword are ignored.
+
+           string    The string to parse. Should not contain a newline char
+           comment_chars    Comment characters to look for.
+           keywords         Keywords to look for, which start a QSYS line.
+        """
+        for line in f:
+            self.parse_line(line,comment_chars=comment_chars, keywords=keywords)
+
+    def parse_line(self,string,comment_chars=["#"], keywords=["QSYS"]):
+        """Parse the values contained in the given line of text
+           and place the values into the data object, which was provided
+           upon construction.
+           Only values which are Null will not be set.
+
+           All characters before comment_char + keyword or comment_char + " "
+           + keyword are ignored.
+
+           string    The string to parse. Should not contain a newline char
+           comment_chars    Comment characters to look for.
+           keywords         Keywords to look for, which start a QSYS line.
+        """
+        # Build the startstrings like "#QSYS", "# QSYS"
+        startstrings = [ v for c in comment_chars for k in keywords for v in (c+k,c+' '+k) ]
+
+        # Find a keyword in the line:
+        start=-1
+        for startstr in startstrings:
+            start=string.find(startstr)
+            if start >= 0:
+                start = start + len(startstr)
+                break
+        if start == -1: return
+
+        data=self.__data
+        vals = qsys_line.__parse_to_key_value(string[start:])
+        for k in vals:
+            verror_string = ("Could not parse value \""+vals[k]
                 + "\" of QSYS directive " + k 
                 +". Check that you supplied something sensible.")
+            ignore_string = ("Warning: Ignoring "+available_directives[k]
+                + " (== " + vals[k] + ") "
+                + "specified in input file "
+                + "via \"QSYS "+k+"=\", "
+                + "since already provided (probably via the commandline).")
 
-            if not k in qsys_line.available_directives:
+            if not k in available_directives:
+                print("Warning: Ignoring unknown QSYS directive " + k
+                        + "( == " + vals[k] + ").")
                 continue
             elif k == "wt":
                 if data.walltime is None:
                     try:
                         data.walltime = \
-                            utils.interpret_string_as_time_interval(self.__vals[k])
+                            utils.interpret_string_as_time_interval(vals[k])
                     except ArgumentParser:
                         raise ValueError(verror_string)
                 else:
-                    self.__print_ignore(k)
+                    print(ignore_string)
             elif k == "np":
                 if data.no_nodes() == 0:
                     try:
                         node = node_type()
-                        node.no_procs = int(self.__vals[k])
+                        node.no_procs = int(vals[k])
                         data.add_node_type(node)
                     except ValueError:
                         raise ValueError(verror_string)
                 else:
-                    self.__print_ignore(k)
+                    print(ignore_string)
             elif k == "mem":
                 if data.physical_memory is None:
                     try:
                         data.physical_memory = \
-                            utils.interpret_string_as_file_size(self.__vals[k])
+                            utils.interpret_string_as_file_size(vals[k])
                     except ArgumentParser:
                         raise ValueError(verror_string)
                 else:
-                    self.__print_ignore(k)
+                    print(ignore_string)
             elif k == "vmem":
                 if data.virtual_memory is None:
                     try:
                         data.virtual_memory = \
-                            utils.interpret_string_as_file_size(self.__vals[k])
+                            utils.interpret_string_as_file_size(vals[k])
                     except ArgumentParser:
                         raise ValueError(verror_string)
                 else:
-                    self.__print_ignore(k)
-            else:
-                raise ValueError("Unknown QSYS directive: \""+k+"\"")
+                    print(ignore_string)
 
-    """
-    The dict mapping from a QSYS directive
-    to a short description what it does.
-    """
-    available_directives = {
-            "wt": "job walltime",
-            "np": "number of processors",
-            "mem": "physical memory",
-            "vmem": "virtual memory",
-            }
+"""
+The dict mapping from a QSYS directive
+to a short description what it does.
+"""
+available_directives = {
+        "wt": "job walltime",
+        "np": "number of processors",
+        "mem": "physical memory",
+        "vmem": "virtual memory",
+        }
